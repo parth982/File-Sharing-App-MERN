f82c8d72729c5e8c49cb64d6409608b93493a459
Parth_Ghatge <parthghatge982@gmail.com>
2023-08-23 19:54:13 +0530

    Completed Version 1.0

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -15,6 +15,7 @@ import {
 
 const Home = () => {
   const [fileLink, setFileLink] = useState(null);
+  const [isFileUploaded, setIsFileUploaded] = useState(false);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -28,46 +29,75 @@ const Home = () => {
         formData
       );
       setFileLink(response.data.path);
+      setIsFileUploaded(true);
     } catch (error) {
       console.error("Error uploading file", error);
     }
   };
 
   return (
-    <Center minHeight="100vh">
+    <Center minHeight="100vh" bg="gray.100">
       <Box
         p={8}
         borderWidth={1}
-        borderRadius="md"
-        boxShadow="lg"
+        borderRadius="lg"
+        boxShadow="xl"
         width="100%"
-        maxWidth="400px"
+        maxWidth="500px"
+        bg="white"
       >
         <Stack spacing={4}>
-          <Heading size="lg">File Sharing App</Heading>
+          <Heading size="xl" textAlign="center" color="red.700">
+            File Sharing App
+          </Heading>
           {fileLink && (
-            <Box>
-              <Text fontSize="md" mb={2}>
-                Your file is uploaded.{" "}
-                <Link href={fileLink} isExternal color="teal.500">
-                  {fileLink}
-                </Link>
+            <Box display={"flex"} flexDir={"column"}>
+              <Text fontSize="md" textAlign="center" mb={2}>
+                Your file has been successfully uploaded.
               </Text>
+              <Link
+                textAlign="center"
+                href={fileLink}
+                isExternal
+                color="teal.500"
+              >
+                {fileLink}
+              </Link>
             </Box>
           )}
           <form onSubmit={handleFormSubmit}>
             <FormControl>
-              <FormLabel htmlFor="file">Click Here to Choose a file:</FormLabel>
+              <FormLabel htmlFor="file">
+                <Button
+                  as="label"
+                  htmlFor="file"
+                  cursor="pointer"
+                  colorScheme={"whatsapp"}
+                  size="lg"
+                  w="100%"
+                  transition="background-color 0.3s, color 0.3s"
+                >
+                  {isFileUploaded ? "Upload Another File" : "Upload a File"}
+                </Button>
+              </FormLabel>
               <Input
                 type="file"
                 id="file"
                 name="file"
-                display={"none"}
+                display="none"
                 required
               />
             </FormControl>
-            <Button mt={2} type="submit" colorScheme="teal" size="md">
-              Share
+            <Button
+              mt={4}
+              type="submit"
+              colorScheme="messenger"
+              size="lg"
+              w="100%"
+              disabled={isFileUploaded}
+              transition="background-color 0.3s, color 0.3s"
+            >
+              Get Download Link
             </Button>
           </form>
         </Stack>
